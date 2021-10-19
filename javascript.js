a501704e5fb3d60b6ef12b4e6ee1a5a41511a4d6
Menelantir <91024559+Menelantir@users.noreply.github.com>
2021-10-19 17:50:22 +0200

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,57 @@
+var plus = document.querySelectorAll(".plus");
+var minus = document.querySelectorAll(".minus");
+var remove = document.querySelectorAll(".remove");
+var like = document.querySelectorAll(".like");
+
+
+
+
+for (let i = 0; i<plus.length; i++) {
+plus[i].addEventListener("click", function (){
+plus[i].nextElementSibling.innerHTML++;
+Total()
+})
+}
+
+for(let i = 0; i<minus.length; i++) {
+minus[i].addEventListener("click", function () {
+if (minus[i].previousElementSibling.innerHTML > 0)
+minus[i].previousElementSibling.innerHTML--;
+Total()
+})
+}
+
+
+for (let i=0; i<remove.length; i++) {
+remove[i].addEventListener("click", function() {
+remove[i].parentNode.parentNode.remove();
+Total()
+})
+}
+
+for (let i = 0 ; i<like.length; i++) {
+like[i].addEventListener("click", function() {
+like[i].classList.toggle("red");
+alert("Product added to wishlist!")
+
+})
+}
+
+
+function Total() {
+var price = document.querySelectorAll(".price");
+var quantity = document.querySelectorAll(".number");
+var total = 0;
+var totamount = document.querySelector(".totamount")
+for (var i = 0; i < quantity.length; i++) {
+
+total += parseInt(price[i].innerText) * parseInt(quantity[i].innerText)
+}
+totamount.innerHTML = total
+
+}
+
+
+
+
+
